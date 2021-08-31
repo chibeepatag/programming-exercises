762529d83126470e744d0e8df36fabf1523c7cc2
cpatag <cpatag@iripple.net>
2021-08-31 10:15:27 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ruby-exercices
 Ruby Exercises for Practice
+
+## This is a collection of chibeepatag's answers to programming exercises. Most of the exercises here were found on exercism.io or leetcode
+
+## Ruby
+* zigzag.rb
---
+++ zigzag.rb
@@ -0,0 +1,39 @@
+def convert(s, num_rows)
+  rows = initialize_rows(num_rows)
+  final_pattern = pattern(s, num_rows)
+  0.upto(s.length-1).each do |i|
+      current_char = s[i]
+      row = final_pattern[i]
+      rows[row] << current_char
+  end
+  answer = ""
+  rows.values.each do |array|
+    array.each do |char|
+      answer << char
+    end
+  end
+  return answer
+end
+def initialize_rows(num_rows)
+  rows = {}
+    0.upto(num_rows - 1).each do |i|
+    rows[i] = []
+  end
+  return rows
+end
+def pattern(s, num_rows)
+  pattern_arr = []
+  0.upto(num_rows - 1).each do |i|
+    pattern_arr << i
+  end
+  (num_rows - 2).downto(1).each do |i|
+    pattern_arr << i
+  end
+  cycle = pattern_arr.cycle
+  final_pattern = []
+  (1..s.length).each do |i|
+    final_pattern << cycle.next
+  end
+  return final_pattern
+end
+convert("PAYPALISHIRING", 3)
